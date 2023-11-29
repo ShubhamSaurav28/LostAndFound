efcaecb67f17e7caf595f10608c10addbb2c8c4a
Shubham Saurav <ssaurav28502@gmail.com>
2023-11-29 17:09:42 +0530

    server change

--- server/connectDB.js
+++ server/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = ()=>{ mongoose.connect('mongodb+srv://ShubhamSaurav:testing1234@cluster0.sajvfjq.mongodb.net/?retryWrites=true&w=majority')
+const connectDB = ()=>{ mongoose.connect('mongodb://localhost:27017/lostfound')
 .then(()=>{
     console.log("DB Connected");
 })
@@ -10,3 +10,4 @@ const connectDB = ()=>{ mongoose.connect('mongodb+srv://ShubhamSaurav:testing123
 }
 
 module.exports = connectDB;
+// mongodb+srv://ShubhamSaurav:testing1234@cluster0.sajvfjq.mongodb.net/?retryWrites=true&w=majority
