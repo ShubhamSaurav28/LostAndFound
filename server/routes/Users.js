a69f96cc15b7e5193defe1377ac113e5ffd7cd54
Shubham Saurav <ssaurav28502@gmail.com>
2023-11-30 18:30:44 +0530

    bcrypt to bcryptjs

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 const User = require('../models/User');
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
@@ -32,7 +32,7 @@ router.post('/api/register', async (req,res) => {
         username,
         useremail,
         phoneno,
-        password:bcrypt.hashSync(password,salt),
+        password: await bcrypt.hash(password,salt),
       });
       res.status(200).json(userDoc);
     } catch(e) {
@@ -50,7 +50,7 @@ router.post('/api/register', async (req,res) => {
       res.status(400).json({ error: 'User not found' });
       return;
     }
-    const passOk = bcrypt.compareSync(password,userDoc.password);
+    const passOk = await bcrypt.compare(password,userDoc.password);
     const username = userDoc.username;
     const phoneno = userDoc.phoneno;
     console.log(password);
@@ -85,125 +85,6 @@ router.post('/api/register', async (req,res) => {
   router.post('/api/logout', (req,res) => {
     res.cookie('token', '').json('ok');
   });
-
-
-
-
-// router.get('/',verifytoken,(req, res)=>{
-//     res.render('index');
-// })
-
-// router.get('/register', async (req, res)=>{
-//     const token = req.cookies.token
-
-//     if(!token){
-//         return res.render('register');
-//     }
-
-//     res.redirect("/");
-// })
-
-
-// router.post('/register', async (req, res)=>{
-//     const {username , useremail, password} = req.body;
-//     if(!(username && useremail && password))
-//     {
-//         console.log("All Fields Are Necessary")
-//         return res.status(400).send("All Fields are necessary");
-//     }
-//     const existingUser=await User.findOne({useremail});
-//     if(existingUser)
-//     {
-//         console.log("Already exists")
-//         return res.status(401).send("User With This Name Already Exists");
-//     }
-//     const salt = await bcrypt.genSalt(5)
-//     const hashedPassword = await bcrypt.hashSync(password,salt)
-//     const newUser = await User.create({username,useremail,password:hashedPassword});
-//     return res.status(200).json(newUser);
-// })
-
-
-// router.post('/login', async (req, res)=>{
-//     try{
-//         const {useremail,password}=req.body;
-//         if(!(useremail && password))
-//         {
-//             console.log("All Fields Are Necessary")
-//             return res.status(400).send("All details are necessary")
-//         }
-//         const user = await User.findOne({useremail:req.body.useremail})
-//         if(!user){
-//             console.log("Not exists")
-//             return res.status(404).json("User not found!")
-//         }
-//         const match = await bcrypt.compare(req.body.password,user.password)
-
-//         if(!match){
-//             return res.status(401).json("Wrong credentails!")
-//         }
-//         console.log("1")
-//         const token = jwt.sign({_id:user._id,username:user.username,useremail:user.useremail},'shub',{expiresIn:"3d"})
-//         user.password=undefined;
-//         console.log('2')
-//         res.cookie("token",token);
-//         return res.status(200).json({
-//             success:true,
-//             token,
-//             user
-//         });
-//     }
-//     catch(err){
-//         console.log("Password is incorrect");
-//         res.status(500).json(err);
-//     }
-// })
-
-
-// router.get('/login', async (req, res)=>{
-//     const token = req.cookies.token
-
-//     if(!token){
-//         return res.render('login');
-//     }
-
-//     res.redirect("/");
-    
-// })
-// router.get('/profile', async (req,res) => {
-//     console.log('stt');
-//     const {token} = req.cookies;
-//     if (!token) {
-//         return res.status(401).json({ error: 'JWT is missing' });
-//       }
-//     console.log('a')
-//     jwt.verify(token,"shub", {}, (err,info) => {
-//       if (err) throw err;
-//       res.json(info);
-//     });
-//     console.log('rt')
-//   });
-
-
-
-
-// // logout route
-
-// router.get('/logout',verifytoken,(req,res)=>{
-//     res.clearCookie("token")
-//     res.redirect('/login')
-//     res.end()
-// })
-
-
-//abhi key liye baad me change karna hai
-// router.post('/logout',(req,res)=>{
-//     res.clearCookie("token")
-//     res.redirect('/login')
-//     res.end()
-// })
-
-
 
 
 
