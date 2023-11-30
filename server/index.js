360848c26d96d4c61e1116db2e761046adbf9776
Shubham Saurav <ssaurav28502@gmail.com>
2023-11-30 19:46:56 +0530

    refresh not working

--- server/index.js
+++ server/index.js
@@ -21,10 +21,6 @@ app.get('/',(req,res)=>{
     res.send("working")
 })
 
-app.get('*', (req, res) => {
-    res.send("hi");
-    // res.sendFile(path.resolve(__dirname, 'index.html'));
-  });
 
 connectDB();
 
